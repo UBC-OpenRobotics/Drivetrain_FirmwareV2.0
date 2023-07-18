bb753b2dc8366fd02bdcc558ca5f5be169fddce7
Sofiya Spolitak <sofiyaspolitak12@gmail.com>
2023-07-18 00:34:23 -0700

    added blink state and software timer

--- Modules/inc/LED.h
+++ Modules/inc/LED.h
@@ -2,13 +2,15 @@
 #define LED_H
 
 #include "stm32h7xx_hal.h"
+//osTimerDef(periodic_blink, toggle);   // when the timer expires, the function toggle_power is called
 
 namespace STM32LED
 {
     enum class state
     {
         on,
-        off
+        off,
+        blink
     };
 
     class LED
@@ -17,6 +19,12 @@ namespace STM32LED
             LED(GPIO_TypeDef* gpio_port, uint16_t gpio_pin);
             void set(state s);
             void toggle();
+            void toggle_callback();
+            void timer_init();
+            osTimerId blink_id;
+            osStatus status;// Pleease defiene this in the class as a private attributes, 
+            osTimerDef(periodic_blink, toggle);   // when the timer expires, the function toggle_power is called
+            
             state LED_state;
 
         protected:
@@ -27,6 +35,8 @@ namespace STM32LED
     extern LED* LED1;
     extern LED* LED2;
     extern LED* LED3;
+    //setmode()should be in here 
+    void setMode(LED * LED1, LED * LED2, LED* LED3, state s);
 }
 
 #endif
