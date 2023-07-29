40989bd1b390a05c858ea1d8058cf43b73fc0e27
root <root@JamesG14>
2023-07-29 15:11:41 -0700

    code that defines a class for motorcontrolling, as well as methods to control the assigned hardware timer.

---
+++ Modules/inc/motorControl.h
@@ -0,0 +1,39 @@
+/*Don't know what these do, but they were in other files, so I assume they
+have some importance*/
+#ifndef motorControl_H
+#define motorControl_H
+
+/*STM32 library*/
+#include "stm32h7xx_hal.h"
+#include "stm32h7xx_hal_tim.h"
+
+/* namespace and class defining for motorControl */
+namespace STM32_motorControl
+{
+    class motorControl
+    {
+        public:
+            /* Defining attributes/fields
+            motorC = motor control */
+            GPIO_TypeDef* motorC_port;
+            uint16_t motorC_pin;
+
+            /* Contstuctor for port and pin assignment/definition */
+            motorControl(GPIO_TypeDef* gpio_port, uint16_t gpio_pin, TIM_HandleTypeDef* htim_handle);
+
+            void timer_start();
+            void timer_stop();
+            void adjust_timer_frequency(uint16_t freqHz);
+            void calculate_timer_frequency(uint16_t motor_rpm);
+
+        protected:  
+            const uint16_t motorSteps = 200; //Number of steps per revolution; defined by the hardware stepper motor driver
+            uint32_t timer_freq;
+            TIM_HandleTypeDef*   htim_handle; //Hardware timer handle
+
+    };
+
+
+}
+
+#endif
---
+++ Modules/src/motorControl.cpp
@@ -0,0 +1,39 @@
+#include "motorControl.h"
+
+namespace STM32_motorControl
+{   
+
+    //Constructor 
+    motorControl::motorControl(GPIO_TypeDef* gpio_port, uint16_t gpio_pin, TIM_HandleTypeDef* htim_handle)
+    {
+        this -> motorC_port = gpio_port;
+        this -> motorC_pin = gpio_pin;
+        this -> htim_handle = htim_handle;
+    }
+
+
+    void motorControl::timer_start()
+    {
+        HAL_TIM_Base_Start(this -> htim_handle);
+    }
+
+    void motorControl::timer_stop()
+    {
+        HAL_TIM_Base_Stop(this -> htim_handle);
+    }
+
+    
+    void motorControl::adjust_timer_frequency(uint16_t freqHz)
+    {
+        uint16_t timer_reload_count = 0;
+        //Calcualte the reload value based on the frequency parameter.
+        __HAL_TIM_SET_AUTORELOAD(this -> htim_handle, timer_reload_count);
+    }
+
+
+    void motorControl::calculate_timer_frequency(uint16_t motor_rpm)
+    {
+        this -> timer_freq = motor_rpm*this -> motorSteps*(1/60);
+    }
+
+}
